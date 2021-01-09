764b737c9a56635a863e72c5a6f3401190672025
Ana-MariaZanceanu <ana.zanceanu@yahoo.com>
2021-01-09 15:15:08 +0200

    query with one operator (initial version)

--- AIProject/API/api.py
+++ AIProject/API/api.py
@@ -1,112 +1,93 @@
+from flask import request, jsonify
 from google.cloud import language_v1
+from googletrans import Translator
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= os.path.abspath("ProiectTw-12bf250f4662.json")
+import flask
+import requests
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.abspath("ProiectTw-12bf250f4662.json")
+
+
+operators_sim = dict()
+translator = Translator()
+
 
 def sample_analyze_entities(text_content):
     client = language_v1.LanguageServiceClient()
     type_ = language_v1.Document.Type.PLAIN_TEXT
     language = "en"
     document = {"content": text_content, "type_": type_, "language": language}
-    encoding_type = language_v1.EncodingType.UTF8
-    response = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type})
-    for entity in response.entities:
-        print(u"Representative name for the entity: {}".format(entity.name))
+    response = client.analyze_entities(request={'document': document})
+    return response.entities
 
-        # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
-        print(u"Entity type: {}".format(language_v1.Entity.Type(entity.type_).name))
-
-        # Get the salience score associated with the entity in the [0, 1.0] range
-        print(u"Salience score: {}".format(entity.salience))
-
-        # Loop over the metadata associated with entity. For many known entities,
-        # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
-        # Some entity types may have additional metadata, e.g. ADDRESS entities
-        # may have metadata for the address street_name, postal_code, et al.
-        for metadata_name, metadata_value in entity.metadata.items():
-            print(u"{}: {}".format(metadata_name, metadata_value))
-
-        # Loop over the mentions of this entity in the input document.
-        # The API currently supports proper noun mentions.
-        for mention in entity.mentions:
-            print(u"Mention text: {}".format(mention.text.content))
-
-            # Get the mention type, e.g. PROPER for proper noun
-            print(
-                u"Mention type: {}".format(language_v1.EntityMention.Type(mention.type_).name)
-            )
-
-    # Get the language of the text, which will be the same as
-    # the language specified in the request or, if not specified,
-    # the automatically-detected language.
-    print(u"Language of the text: {}".format(response.language))
-
-from google.cloud import language_v1
 
 def sample_analyze_syntax(text_content):
-    """
-    Analyzing Syntax in a String
-
-    Args:
-      text_content The text content to analyze
-    """
-
     client = language_v1.LanguageServiceClient()
-
-    # text_content = 'This is a short sentence.'
-
-    # Available types: PLAIN_TEXT, HTML
     type_ = language_v1.Document.Type.PLAIN_TEXT
-
-    # Optional. If not specified, the language is automatically detected.
-    # For list of supported languages:
-    # https://cloud.google.com/natural-language/docs/languages
     language = "en"
     document = {"content": text_content, "type_": type_, "language": language}
-
-    # Available values: NONE, UTF8, UTF16, UTF32
     encoding_type = language_v1.EncodingType.UTF8
+    response = client.analyze_syntax(request={'document': document, 'encoding_type': encoding_type})
+    return response
 
-    response = client.analyze_syntax(request = {'document': document, 'encoding_type': encoding_type})
-    # Loop through tokens returned from the API
-    print(response)
-    for token in response.tokens:
-        # Get the text content of this token. Usually a word or punctuation.
-        text = token.text
-        print(u"Token text: {}".format(text.content))
-        print(
-            u"Location of this token in overall document: {}".format(text.begin_offset)
-        )
-        # Get the part of speech information for this token.
-        # Parts of spech are as defined in:
-        # http://www.lrec-conf.org/proceedings/lrec2012/pdf/274_Paper.pdf
-        part_of_speech = token.part_of_speech
-        # Get the tag, e.g. NOUN, ADJ for Adjective, et al.
-        print(
-            u"Part of Speech tag: {}".format(
-                language_v1.PartOfSpeech.Tag(part_of_speech.tag).name
-            )
-        )
-        # Get the voice, e.g. ACTIVE or PASSIVE
-        print(u"Voice: {}".format(language_v1.PartOfSpeech.Voice(part_of_speech.voice).name))
-        # Get the tense, e.g. PAST, FUTURE, PRESENT, et al.
-        print(u"Tense: {}".format(language_v1.PartOfSpeech.Tense(part_of_speech.tense).name))
-        # See API reference for additional Part of Speech information available
-        # Get the lemma of the token. Wikipedia lemma description
-        # https://en.wikipedia.org/wiki/Lemma_(morphology)
-        print(u"Lemma: {}".format(token.lemma))
-        # Get the dependency tree parse information for this token.
-        # For more information on dependency labels:
-        # http://www.aclweb.org/anthology/P13-2017
-        dependency_edge = token.dependency_edge
-        print(u"Head token index: {}".format(dependency_edge.head_token_index))
-        print(
-            u"Label: {}".format(language_v1.DependencyEdge.Label(dependency_edge.label).name)
-        )
 
-    # Get the language of the text, which will be the same as
-    # the language specified in the request or, if not specified,
-    # the automatically-detected language.
-    print(u"Language of the text: {}".format(response.language))
+def get_similarities_for_word(word):
+    url = 'https://api.datamuse.com/words?ml=' + word
+    response = requests.get(url)
+    similarities_list = response.json()
+    return [item['word'] for item in similarities_list]
 
-sample_analyze_syntax("Map from Silicon Valley to San Francisco")
-sample_analyze_entities("Map Silicon Valley to San Francisco")
+
+def get_similarities_for_operators():
+    operators_list = ['define', 'filetype', 'site', 'related', 'weather', 'stocks', 'map', 'movie', 'location']
+    for operator in operators_list:
+        operators_sim[operator] = get_similarities_for_word(operator)
+
+
+def get_similarity_for_word(word):
+    word = word.replace(' ', '')
+    if word in operators_sim.keys():
+        return word
+    for operator in operators_sim.keys():
+        if word in operators_sim[operator]:
+            return operator
+    return None
+
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+get_similarities_for_operators()
+
+
+@app.route('/', methods=['POST'])
+def create_task():
+    text_ro = request.json["text"]
+    translation = translator.translate(text_ro, dest='en')
+    text_en = translation.text
+    response_entities = sample_analyze_entities(text_en)
+    print(response_entities)
+    query = ''
+    entities_types = []
+    entity_dict = {
+        'operator': '',
+        'operator_value': ''
+    }
+    for entity in response_entities:
+        entity_operator_name = get_similarity_for_word(str(entity.name).lower())
+        entity_operator_type = get_similarity_for_word(str(entity.type_)[5:].lower())
+        if entity_operator_name and entity_dict.get('operator') == '':
+            entity_dict['operator'] = entity_operator_name
+        else:
+            if entity_operator_type and entity_dict.get('operator') == '':
+                entity_dict['operator'] = entity_operator_type
+                # in special pentru location; daca exista o singura entitate, care are tipul location..atunci operatorul este location (gasit in operatori) iar valoarea este entity.name:
+                if len(response_entities) == 1:
+                    entity_dict['operator_value'] = entity.name
+            else:
+                entity_dict['operator_value'] = entity.name
+    entities_types.append(entity_dict)
+    for entity_type in entities_types:
+        query = entity_type['operator'] + ':' + entity_type['operator_value']
+    return jsonify({'response': query})
+
+
+app.run()
